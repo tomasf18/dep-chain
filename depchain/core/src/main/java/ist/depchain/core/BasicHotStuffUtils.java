4ca021b839872484e8345360de49557fd92ef8d9
andrepires2211 <andre.nobre.pires@gmail.com>
2026-02-28 16:18:43 +0000

    Updated HotStuff utils

--- depchain/core/src/main/java/ist/depchain/core/BasicHotStuffUtils.java
+++ depchain/core/src/main/java/ist/depchain/core/BasicHotStuffUtils.java
@@ -1,107 +1,109 @@
-// package ist.depchain.core;
+package ist.depchain.core;
 
-// import com.google.protobuf.ByteString;
-// import ist.depchain.common.HotStuffMessage;
-// import ist.depchain.common.HotStuffMessage.Type;
-// import ist.depchain.common.Block;
-// import ist.depchain.common.QC;
-// import ist.depchain.common.Command;
+import com.google.protobuf.ByteString;
+import ist.depchain.common.HotStuffMessage;
+import ist.depchain.common.HotStuffMessage.Type;
+import ist.depchain.common.Block;
+import ist.depchain.common.QC;
+import ist.depchain.common.Command;
 
-// import java.util.Collection;
+ import java.util.Collection;
 
-// public class BasicHotStuffUtils {
+ public class BasicHotStuffUtils {
 
-//     private final BlockStorage storage;
+     private final BlockStorage storage;
 
-//     public BasicHotStuffUtils() {
-//         storage = new BlockStorage();
-//     }
+     public BasicHotStuffUtils() {
+         storage = new BlockStorage();
+     }
 
-//     /* MESSAGES */
+     /* MESSAGES */
 
-//     // [Line 1-6] - Create base HotStuff Message
-//     public HotStuffMessage msg(Type type, int viewNumber, Block node, QC justify) {
-//         return HotStuffMessage.newBuilder()
-//                 .setType(type)                // [Line 2]
-//                 .setViewNumber(viewNumber)    // [Line 3]
-//                 .setNode(node)                // [Line 4]
-//                 .setJustify(justify)          // [Line 5]
-//                 .build();
-//     }
+     // [Line 1-6] - Create base HotStuff Message
+     public HotStuffMessage msg(Type type, int viewNumber, Block node, QC justify) {
+         return HotStuffMessage.newBuilder()
+                 .setType(type)                // [Line 2]
+                 .setViewNumber(viewNumber)    // [Line 3]
+                 .setNode(node)                // [Line 4]
+                 .setJustify(justify)          // [Line 5]
+                 .build();
+     }
 
-//     // [Line 7-10] - Create Vote Message
-//     public HotStuffMessage voteMsg(Type type, int viewNumber, Block node,  QC justify, byte[] partialSig) {
-//         // [Line 8] - m <- Msg(type, node, qc)
-//         HotStuffMessage m = msg(type, viewNumber, node, justify);
+     // [Line 7-10] - Create Vote Message
+     public HotStuffMessage voteMsg(Type type, int viewNumber, Block node,  QC justify, byte[] partialSig) {
+         // [Line 8] - m <- Msg(type, node, qc)
+         HotStuffMessage m = msg(type, viewNumber, node, justify);
 
-//         // [Line 9-10] - m.partialSig <- tsignr(<m.type, m.viewNumber, m.node>)
-//         return m.toBuilder()
-//                 .setPartialSig(ByteString.copyFrom(partialSig))
-//                 .build();
-//     }
+         // [Line 9-10] - m.partialSig <- tsignr(<m.type, m.viewNumber, m.node>)
+         return m.toBuilder()
+                 .setPartialSig(ByteString.copyFrom(partialSig))
+                 .build();
+     }
 
-//     /* TREE & QC */
+     /* TREE & QC */
 
-//     // [Line 11-14] - Create a new block (LEAF)
-//     public Block createLeaf(Block parent, Command cmd, byte[] hash){
-//         return Block.newBuilder()
-//                 .setParentId(parent.getId())
-//                 .setCmd(cmd)
-//                 .setHash(ByteString.copyFrom(hash))
-//                 .build();
-//     }
+     // [Line 11-14] - Create a new block (LEAF)
+     public Block createLeaf(Block parent, Command cmd, int id){
+         return Block.newBuilder()
+                 .setParentId(parent.getId())
+                 .setCmd(cmd)
+                 .setId(id)
+                 .build();
+     }
 
-//     // [Line 15-20] - Create QC
-//     public QC createQC(Collection<HotStuffMessage> v, byte[] combinedSig) {
-//         if ( v.isEmpty() ) {return null;}
-//         HotStuffMessage msg = v.iterator().next();
+     // [Line 15-20] - Create QC
+     public QC createQC(Collection<HotStuffMessage> v, byte[] combinedSig) {
+         if ( v.isEmpty() ) {return null;}
+         HotStuffMessage msg = v.iterator().next();
 
-//         return QC.newBuilder()
-//                 .setType(msg.getType())                     // [Line 16]
-//                 .setViewNumber(msg.getViewNumber())         // [Line 17]
-//                 .setNodeId(msg.getNode().getId())       // [Line 18]
-//                 .setSig(ByteString.copyFrom(combinedSig))   // [Line 19]
-//                 .build();
-//     }
+         return QC.newBuilder()
+                 .setType(msg.getType())                     // [Line 16]
+                 .setViewNumber(msg.getViewNumber())         // [Line 17]
+                 .setNodeId(msg.getNode().getId())       // [Line 18]
+                 .setSig(ByteString.copyFrom(combinedSig))   // [Line 19]
+                 .build();
+     }
 
-//     /* MATCHING & SAFETY FUNCTIONS */
+     /* MATCHING & SAFETY FUNCTIONS */
 
-//     // [Line 21-22] - Verify if Message is the same
-//     public boolean matchingMSG(HotStuffMessage m, Type t, int v) {
-//         return m.getType() == t && m.getViewNumber() == v;
-//     }
+     // [Line 21-22] - Verify if Message is the same
+     public boolean matchingMSG(HotStuffMessage m, Type t, int v) {
+         return m.getType() == t && m.getViewNumber() == v;
+     }
 
-//     // [Line 23-24] - Verify if QC coincides with the expected type and view number
-//     public boolean matchingQC(QC qc, Type t, int v){
-//         return qc.getType() == t && qc.getViewNumber() == v;
-//     }
+     // [Line 23-24] - Verify if QC coincides with the expected type and view number
+     public boolean matchingQC(QC qc, Type t, int v){
+         return qc.getType() == t && qc.getViewNumber() == v;
+     }
 
-//     // [Line 26-27] - Check if node is safe
-//     public boolean safeNode(Block node, QC qc, QC lockedQC){
-//         //[Line 26]
-//         boolean extendsLocked = extendsFrom(node, lockedQC.getNodeHash());
+     // [Line 26-27] - Check if node is safe
+     public boolean safeNode(Block node, QC qc, QC lockedQC){
+         if(lockedQC == null || lockedQC.getViewNumber() == 0) return true;
 
-//         //[Line 27]
-//         boolean viewIsHigher = qc.getViewNumber() > lockedQC.getViewNumber();
+         //[Line 26]
+         boolean extendsLocked = extendsFrom(node, lockedQC.getNodeId());
 
-//         return extendsLocked || viewIsHigher;
-//     }
+         //[Line 27]
+         boolean viewIsHigher = qc.getViewNumber() > lockedQC.getViewNumber();
 
-//     /* HELPER FUNCTIONS */
+         return extendsLocked || viewIsHigher;
+     }
 
-//     // [NEW FUNCTION] - Verify if "node" has antecessor "targethash"
-//     public boolean extendsFrom(Block node, int targetId){
-//         if(node.getId().equals(targetId)){return true;}
+     /* HELPER FUNCTIONS */
 
-//         int currentParentHash = node.getParentId();
-//         while(currentParentHash != null && !currentParentHash.isEmpty()){
-//             if(currentParentHash.equals(targetHash)){return true;}
+     // [NEW FUNCTION] - Verify if "node" has antecessor "targethash"
+     public boolean extendsFrom(Block node, int targetId){
+         if(node.getId() == targetId){return true;}
 
-//             Block parent = storage.getBlock(currentParentHash);
-//             if(parent == null){break;}
-//             currentParentHash = parent.getParent();
-//         }
-//         return false;
-//     }
+         int currentParentId = node.getParentId();
+         while(currentParentId != 0){
+             if(currentParentId == targetId){return true;}
 
-// }
+             Block parent = storage.getBlock(currentParentId);
+             if(parent == null){break;}
+             currentParentId = parent.getParentId();
+         }
+         return false;
+     }
+
+}
