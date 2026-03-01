42d834d0bdb06c57086ab6297551ab75e3db5c53
andrepires2211 <andre.nobre.pires@gmail.com>
2026-03-01 21:33:42 +0000

    Added Early version of Prepare Phase

--- depchain/core/src/main/java/ist/depchain/core/BasicHotStuffProtocol.java
+++ depchain/core/src/main/java/ist/depchain/core/BasicHotStuffProtocol.java
@@ -1,14 +1,18 @@
 package ist.depchain.core;
 
+import com.google.protobuf.ByteString;
+import ist.depchain.common.Block;
+import ist.depchain.common.Command;
+import ist.depchain.common.HotStuffMessage;
 import ist.depchain.common.QC;
 import ist.depchain.core.hotstuff.*;
 
-import java.util.Collection;
-import java.util.Comparator;
+import java.util.*;
 
 public class BasicHotStuffProtocol {
     private final BasicHotStuffUtils utils;
     private final BlockStorage storage;
+    private final ServerContext serverContext;
     private int n, f;
     private int curView = 1;
 
@@ -16,14 +20,87 @@ public class BasicHotStuffProtocol {
     private QC lockedQC = BasicHotStuffUtils.genesisQC;  // [PHASE 3] - The latest QC that was locked
     private QC prepareQC = BasicHotStuffUtils.genesisQC; // [PHASE 1] - The latest prepare QC
 
-    private final String id;
+    // Phase Tracking
+    private final Map<HotStuffMessage.Type, Map<ByteString, Set<HotStuffMessage>>> voteCollector = new HashMap<>();
+    private final Map<Integer, Set<HotStuffMessage>> newViewMsgs = new HashMap<>();
+    private final Queue<Command> commandMempool = new LinkedList<>();
 
-    public BasicHotStuffProtocol(String id, int n, int f) {
-        this.id = id;
+    public BasicHotStuffProtocol(ServerContext serverContext, int n, int f) {
+        this.serverContext = serverContext;
         this.n = n;
         this.f = f;
         this.storage = new BlockStorage();
         this.utils = new BasicHotStuffUtils(this.storage);
     }
 
+    public String getLeader(int view){
+        Object[] servers = serverContext.getConfig().getBlockChainServers().keySet().toArray();
+        return (String)servers[view];
+    }
+
+    public boolean isLeader(){
+        String id = serverContext.getConfig().getSelfId();
+        return getLeader(curView).equals(id);
+    }
+
+    /** PREPARE PHASE FOR LEADER **/
+    public void onReceiveNewView(HotStuffMessage m){
+        int view = m.getViewNumber();
+        newViewMsgs.computeIfAbsent(view, k -> new HashSet<>()).add(m);
+
+        if(newViewMsgs.size() >= (n-f) && isLeader()){
+            QC highQC = newViewMsgs.get(view).stream()
+                    .map(HotStuffMessage::getJustify)
+                    .max(Comparator.comparingInt(QC::getViewNumber))
+                    .orElse(BasicHotStuffUtils.genesisQC);
+
+            Command command = commandMempool.poll();
+            if(command == null)
+                return;
+
+            ByteString newId = ByteString.copyFromUtf8(UUID.randomUUID().toString());
+            Block curProposal = utils.createLeaf(storage.getBlock(highQC.getBlockId()), command, newId);
+            storage.putBlock(curProposal);
+
+            HotStuffMessage prepareMsg = utils.msg(HotStuffMessage.Type.PREPARE, curView, curProposal, highQC);
+            broadcast(prepareMsg);
+
+            newViewMsgs.remove(view);
+        }
+    }
+
+
+    /** PREPARE PHASE FOR REPLICA **/
+    public void onReceivePrepare(HotStuffMessage m){
+        if(!utils.matchingMSG(m, HotStuffMessage.Type.PREPARE, curView))
+            return;
+
+        Block node = m.getBlock();
+        QC justify = m.getJustify();
+
+        boolean extendsJustify = utils.extendsFrom(node, justify.getBlockId());
+        boolean safeNode = utils.safeNode(node, justify, lockedQC);
+
+        if(extendsJustify && safeNode){
+            storage.putBlock(node);
+
+            byte[] dummySig = new byte[0]; //TODO - Replace with real crypto later
+            HotStuffMessage vote = utils.voteMsg(HotStuffMessage.Type.PREPARE, curView, node, justify, dummySig);
+
+            String leaderId = getLeader(curView);
+            serverContext.getPerfectLink().send(leaderId, vote.toByteArray());
+
+            System.out.println("[REPLICA] - Voted PREPARE for Block: " + node.getId().toStringUtf8());
+        }
+    }
+
+    /** HELPER FUNCTIONS **/
+    public void broadcast(HotStuffMessage m){
+        Set<String> destinations = serverContext.getConfig().getBlockChainServers().keySet();
+        serverContext.getPerfectLink().broadcast(destinations, m.toByteArray());
+    }
+
+    public void addClientCmd(Command cmd){
+        this.commandMempool.add(cmd);
+    }
 }
